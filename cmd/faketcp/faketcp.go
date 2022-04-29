4c660e7a69a77aed72b935b56146a6b8b2814f21
Maksim Lopatin <maksim.lopatin.spb@gmail.com>
2022-04-29 23:50:10 +0300

    Странная ошибка с размером

--- cmd/faketcp/faketcp.go
+++ cmd/faketcp/faketcp.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
-	"net"
+	// "net"
 	"os"
 	"time"
 )
@@ -15,16 +16,18 @@ const (
 )
 
 func main() {
-	StartClient(8081, "./cmd/faketcp/room2.dat")
+	// StartClient(8081, "./cmd/faketcp/D=2020-05-20_T=18-02-50--air.dat")
+	StartClient(8081, "./cmd/faketcp/D=2020-05-20_T=18-03-48--fullbottle05.dat")
+	// StartClient(8081, "./cmd/faketcp/room2.dat")
 }
 
 func StartClient(port int, filepath string) {
 	// Подключаемся к сокету
-	conn, err := net.Dial("tcp", ":"+fmt.Sprint(port))
-	if err != nil {
-		fmt.Println("Неудачная попытка подключения")
-		os.Exit(1)
-	}
+	// conn, err := net.Dial("tcp", ":"+fmt.Sprint(port))
+	// if err != nil {
+	// 	fmt.Println("Неудачная попытка подключения")
+	// 	os.Exit(1)
+	// }
 
 	f, err := os.Open(filepath)
 	if err != nil {
@@ -33,10 +36,15 @@ func StartClient(port int, filepath string) {
 	defer f.Close()
 
 	reader := bufio.NewReader(f)
-	buf := make([]byte, BUF_LEN)
+	bufSize := make([]byte, 2)
 
 	for {
-		n, err := reader.Read(buf)
+		reader.Read(bufSize)
+		packetSize := binary.BigEndian.Uint16(bufSize)
+		buf := make([]byte, packetSize - 0)
+		reader.Read(buf)
+
+		fmt.Println(packetSize)
 
 		if err != nil {
 			if err != io.EOF {
@@ -45,8 +53,7 @@ func StartClient(port int, filepath string) {
 			break
 		}
 
-		conn.Write(buf)
-		fmt.Println("Отправлено байт:", n)
+		// conn.Write(buf)
 		time.Sleep(1000 * time.Millisecond)
 	}
 	fmt.Println("Файл подошел к концу")
