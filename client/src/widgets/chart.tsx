005fe5b4526ba557ad6d6196e57ba424f36d9fda
Maksim L <maksim.lopatin.spb@gmail.com>
2023-07-02 15:32:00 +0300

    декомпозиция (графики)

--- client/src/widgets/chart.tsx
+++ client/src/widgets/chart.tsx
@@ -1,6 +1,13 @@
 import { FC, useEffect, useRef } from 'react'
-import { colors } from 'shared/chart'
 import { WebglPlot, WebglLine } from 'webgl-plot'
+import { ColorRGBA } from 'webgl-plot'
+
+const colors = [
+  {r: 192, g: 57, b: 43},
+  {r: 82, g: 190, b: 128},
+  {r: 84, g: 153, b: 199},
+  {r: 245, g: 176, b: 65},
+].map(c => new ColorRGBA(c.r / 255, c.g / 255, c.b / 255, 1))
 
 const DELIMETER = 150
 const SHIFT = -1
